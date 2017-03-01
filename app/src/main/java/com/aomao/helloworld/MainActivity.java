230e7d13f66f008c23b064e97aefb0fe61e3e8c5
aomao <1585281572@qq.com>
2017-03-01 08:28:08 +0800

    更改内容并提交

--- app/src/main/java/com/aomao/helloworld/MainActivity.java
+++ app/src/main/java/com/aomao/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.aomao.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"嘿嘿！",Toast.LENGTH_SHORT).show();
     }
 }
